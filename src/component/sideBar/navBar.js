9426ff04c4473f6ee2788619326660dea4efab3c
timthebouncer <71633334+timthebouncer@users.noreply.github.com>
2021-09-16 22:48:33 +0800

    svg 管理

--- src/component/sideBar/navBar.js
+++ src/component/sideBar/navBar.js
@@ -2,23 +2,25 @@ import React from 'react'
 import {SidebarData} from './sideBarData'
 import {Link, Route} from 'react-router-dom'
 import './sidebar.css'
+import {userIcon} from '@/Icon/svg'
 import show from "@/component/tooltip/tooltip";
+import tooltip from "@/component/tooltip/tooltip";
 
 const NavBar=({switchMenu, userData})=>{
 
   const tooltip=(e,name)=>{
-    e.preventDefault()
+    console.log(e)
     if(switchMenu){
-      show.toolTip(e.type,name)
-    }
-
-  }
-  const closeToolTip=(e)=>{
-    e.preventDefault()
-    if(switchMenu){
-      show.toolTip(e.type)
+      let element = e.target.getBoundingClientRect()
+      show.toolTip(e.type,name,e)
     }
   }
+  // const closeToolTip=(e)=>{
+  //   e.preventDefault()
+  //   if(switchMenu){
+  //     show.toolTip(e.type)
+  //   }
+  // }
 
   return(
     <div className={switchMenu===false? "w-44 h-screen p-4":"w-11 h-screen p-4"} >
@@ -30,10 +32,8 @@ const NavBar=({switchMenu, userData})=>{
                   return(
                   <li className="sidebar-items" key={index}>
                   <Link className="flex" to={item.path} >
-                  <span onMouseEnter={(e)=>tooltip(e,item.name)} className='mr-4'>
-                    <svg onMouseLeave={(e)=>closeToolTip(e)} xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                    </svg>
+                  <span className='mr-4' onMouseEnter={e=> tooltip(e,item.name)}>
+                  {userIcon}
                   </span>
                         {
                           switchMenu===false? <span>{item.name}</span>:<span className={'opacity-0'}>{item.name}</span>
@@ -45,9 +45,7 @@ const NavBar=({switchMenu, userData})=>{
                           <li className="sidebar-items" key={index}>
                             <Link className="flex" to={item.path}>
                               <span className='mr-4'>
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                                </svg>
+                                {userIcon}
                               </span>
                               {
                                 switchMenu===false? <span>{item.name}</span>:<span></span>
--- src/component/tooltip/tooltip.js
+++ src/component/tooltip/tooltip.js
@@ -2,20 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
-const ToolTip=({name})=>{
+const ToolTip=({name,position})=>{
+    // console.log(position)
     return(
-        <div id={'tooltipId'} className='border-2 border-white shadow-md w-28 absolute top-20 left-12 bg-white'>
+        <div id={'tooltipId'} className={`border-2 border-white shadow-md w-28 bg-white fixed left${-position.left} top${-position.top}`}>
             {name}
         </div>
     )
 }
 
-const showToolTip=(type,name)=>{
+const showToolTip=(type,name,e)=>{
+    let element = e.target.getBoundingClientRect()
+    // console.log(element)
+    // console.log(e.clientY,1)
+    // console.log(e.scrollTop,2)
+    let left = element.left
+    let top = Math.floor(e.clientY - element.top)
 
     if(type === "mouseenter"){
         let div = document.createElement('div')
         document.body.appendChild(div)
-        ReactDOM.render(<ToolTip name={name} />, div)
+        ReactDOM.render(<ToolTip name={name} position={{left,top}} />, div)
     }else {
       let getTooltip =  document.getElementById('tooltipId')
         document.body.removeChild(getTooltip.parentNode)
@@ -26,5 +33,5 @@ const showToolTip=(type,name)=>{
 }
 
 export default {
- toolTip:(type,name)=>showToolTip(type,name)
+ toolTip:(type,name,e)=>showToolTip(type,name,e)
 }
